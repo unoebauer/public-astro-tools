ed78aed43d1e389998ce6b53354ea4a44db71926
Ulrich Noebauer <ulrich.noebauer@gmail.com>
2018-01-29 16:06:36 +0100

    Comment and clean-up eq. diffusion calculator

--- radiative_shock_calculator.py
+++ radiative_shock_calculator.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+import collections
 import numpy as np
 import astropy.constants as c
 import scipy.optimize
@@ -50,9 +51,10 @@ class shock_base(object):
         self.gamma = gamma
         self.kappa = kappa
 
-        self.check_shock_entropy_condition()
+        self._check_shock_entropy_condition()
+        self._calculate_shock_jump()
 
-    def check_shock_entropy_condition(self):
+    def _check_shock_entropy_condition(self):
         """Check whether initial setup satisfies the shock entropy condition
 
         Raises
@@ -71,7 +73,7 @@ class shock_base(object):
         if self.M0 < a0star:
             raise ValueError("Violation of Entropy Condition")
 
-    def calculate_shock_jump(self):
+    def _calculate_shock_jump(self):
         """Calculate overall shock jump
 
         Returns
@@ -100,10 +102,10 @@ class shock_base(object):
             T = x[1]
 
             y = np.zeros(2)
-            # LR07, Eq (12)
+            # LR07, Eq. (12)
             y[0] = ((f1(T) + np.sqrt(f1(T)**2 + f2(T))) /
                     (6. * (self.gamma - 1.) * T) - rho)
-            # LR07, Eq (13)
+            # LR07, Eq. (13)
             y[1] = (3 * rho * (rho * T - 1.) + self.gamma * self.P0 * rho *
                     (T**4 - 1.) - 3. * self.gamma * (rho - 1.) * self.M0**2)
 
@@ -122,26 +124,48 @@ class shock_base(object):
 
 class eqdiff_shock_calculator(shock_base):
     def __init__(self, M0, P0, kappa, gamma=5./3.):
+        """
+        """
         super(eqdiff_shock_calculator, self).__init__(M0, P0, kappa,
                                                       gamma=gamma)
-        self.calculate_shock_jump()
+        # Zel'dovich spike
+        self.Ts = None
+        self.rhos = None
+        self.vs = None
 
-    def precursor_region(self, xpr):
+    def _precursor_region(self, xpr):
+        """Calculate shock state in the precursor region
 
+        The precursor region is at x < 0.
+
+        Parameters
+        ----------
+        xpr : float or np.ndarray
+            dimensionless coordinate (negative per definition)
+
+        Returns
+        -------
+        precursor : collections.namedtuple
+            precursor state, containing dimensionless position, density,
+            temperature and velocity
+        """
+
+        # LE07, Eq. (26)
         def m(T):
             return (0.5 * (self.gamma * self.M0**2 + 1.) +
                     self.gamma * self.P0 / 6. * (1. - T**4))
 
+        # LE07, Eq. (25)
         def rho(T):
             return (m(T) - np.sqrt(m(T)**2 - self.gamma * T * self.M0**2)) / T
 
+        # auxiliary function for LE07, Eq. (24)
         def f3(dens, T):
-            # Note: Missprint in Lowrie & Rauenzahn 2007, eq. 24:
-            # rho has to be replaced by 1!
             return (6. * dens**2 * (T - 1.) / (self.gamma - 1.) +
                     3. * (1. - dens**2) * self.M0**2 +
-                    8. * self.P0 * (T**4 - 1) * dens)
+                    8. * self.P0 * (T**4 - dens) * dens)
 
+        # LE07, Eq. (24)
         def func(T, x):
 
             dens = rho(T)
@@ -149,29 +173,65 @@ class eqdiff_shock_calculator(shock_base):
 
         Tpr = scipy.integrate.odeint(func, self.T1, xpr)
         rhopr = rho(Tpr)
+        # LE07, Eq. (21)
         vpr = self.M0 / rhopr
 
-        return xpr, rhopr, Tpr, vpr
+        result = collections.namedtuple('precursor',
+                                        ['x', 'rho', 'T', 'v'])
+        precursor = result(x=xpr, rho=rhopr, T=Tpr, v=vpr)
 
-    def sample_shock(self, xmin=-0.25, xmax=0.25, N=2000):
+        return precursor
 
-        xpr = np.linspace(0, xmin, N)
+    def sample_shock(self, xmin=-0.25, xmax=0.25, Nsamples=1024):
+        """Sample the shock structure.
+
+        This routine also checks for the existence of a Zel'dovich spike and
+        sets the attributes rhos, vs, Ts.
+
+        Parameters
+        ----------
+        xmin : float
+            dimensionless left limit of domain (default -0.25)
+        xmax : float
+            dimensionless right limit of domain (default 0.25)
+        Nsamples : int
+            number of equidistant points which sample the precursor region
+            (between xmin and 0)
+
+        Returns
+        -------
+        shock : collections.namedtuple
+            shock structure containing the dimensionless position, density,
+            temperature, velocity, Mach number
+        """
+
+        assert(xmin < 0)
+        assert(xmax > 0)
+
+        # Precursor region
+        xpr = np.linspace(0, xmin, Nsamples)
+
+        # Relaxation region; only two points necessary since
         xrel = np.linspace(0, xmax, 2)
 
-        xpr, rhopr, Tpr, vpr = self.precursor_region(xpr)
+        xpr, rhopr, Tpr, vpr = self._precursor_region(xpr)
 
+        # Mach number of p-state; see LR07, Eq. (27d)
         Mpr = vpr[0] / np.sqrt(Tpr[0])
+
+        # checking for the presence of a Zel'dovich spike
         if Mpr > 1.:
-            rhos = (rhopr[0] * (self.gamma + 1.) * Mpr**2 /
-                    (2. + (self.gamma - 1.) * Mpr**2))
-            vs = self.M0 / rhos
-            Ts = (Tpr[0] * (1. - self.gamma + 2. * self.gamma * Mpr**2) *
-                  (2. + (self.gamma - 1.) * Mpr**2) /
-                  ((self.gamma + 1.)**2 * Mpr**2))
-        else:
-            rhos = None
-            vs = None
-            Ts = None
+
+            # Zel'dovich spike density; see LR07, Eq. (27a)
+            self.rhos = (rhopr[0] * (self.gamma + 1.) * Mpr**2 /
+                         (2. + (self.gamma - 1.) * Mpr**2))
+
+            # Zel'dovich spike velocity; see LR07, Eq. (27b)
+            self.vs = self.M0 / self.rhos
+            # Zel'dovich spike temperature; see LR07, Eq. (27c)
+            self.Ts = (Tpr[0] * (1. - self.gamma + 2. * self.gamma * Mpr**2) *
+                       (2. + (self.gamma - 1.) * Mpr**2) /
+                       ((self.gamma + 1.)**2 * Mpr**2))
 
         x = np.append(xpr[::-1], xrel)
         rho = np.append(rhopr[::-1], np.ones(2) * self.rho1)
@@ -179,4 +239,7 @@ class eqdiff_shock_calculator(shock_base):
         v = np.append(vpr[::-1], np.ones(2) * self.v1)
         M = v / np.sqrt(T)
 
-        return x, v, M, rho, T, rhos, Ts, vs
+        result = collections.namedtuple('shock', ['x', 'rho', 'T', 'v', 'M'])
+        shock = result(x=x, rho=rho, T=T, v=v, M=M)
+
+        return shock
