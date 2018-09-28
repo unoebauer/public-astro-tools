2aae2fc9b9229fdc424126299b7fdc7b1633bf92
Ulrich Noebauer <ulrich.noebauer@gmail.com>
2018-09-28 16:19:18 +0200

    Add licence to all python files

--- radiative_shock/radiative_shock_calculator.py
+++ radiative_shock/radiative_shock_calculator.py
@@ -1,13 +1,25 @@
 #!/usr/bin/env python
-import collections
-import numpy as np
-import astropy.constants as c
-import scipy.optimize
-import scipy.integrate
-import matplotlib
-# TODO check for X-server
-matplotlib.use("TkAgg")
-import matplotlib.pyplot as plt
+# MIT License
+#
+# Copyright (c) 2018 Ulrich Noebauer
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
 """
 This module provides tools to calculate the structure of steady radiative
 shocks based on equilibrium and non-equilibrium diffusion using the solution
@@ -24,6 +36,15 @@ References
     LE08: Lowrie & Edwards 2008, Shock Waves (2008) 18:129–143
             DOI 10.1007/s00193-008-0143-0
 """
+import collections
+import numpy as np
+import astropy.constants as c
+import scipy.optimize
+import scipy.integrate
+import matplotlib
+# TODO check for X-server
+matplotlib.use("TkAgg")
+import matplotlib.pyplot as plt
 
 
 a_rad = 4. * c.sigma_sb.cgs.value / c.c.cgs.value
